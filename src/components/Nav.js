4a22ce58b61283edc4c55153a740aa3a64b48bcb
drewleean1 <drewleean@gmail.com>
2023-08-21 09:31:28 -0600

    change nav home

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Nav() {
   return (
     <nav className="appNav">
-        <Link to="/budget">Home</Link>
+        <Link to="/">Home</Link>
         <Link to="../StaffPage">Staff</Link>
         <Link to="../ExpensePage">Expense Log</Link>
     </nav>
