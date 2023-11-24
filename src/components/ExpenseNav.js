9fb236c1412ed496f8a7c8098106f720ebf80436
drewleean1 <drewleean@gmail.com>
2023-11-24 07:31:45 -0700

    change expensenav

--- src/components/ExpenseNav.js
+++ src/components/ExpenseNav.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import LogoutButton from './LogoutButton';
 
 function ExpenseNav() {
   
   return (
-    <nav className = 'ExpenseNav'>
+    <div className="ExpenseHeader" >
+      <nav className="ExpenseNav">
         <Link to="../">Expense</Link>
         <Link to='../CategoryPage/11/2023'>Categories</Link>
         <Link to="../SearchCatPage/11/2023/Travel">Search Category</Link>
-    </nav>
+      </nav>         
+      <LogoutButton />
+    </div>
   );
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LogoutButton from './LogoutButton';
-
 
 function Nav() {
   return (
-    <div className="ExpenseHeader">
-          <nav className="appNav">
+    <nav className="appNav">
         <Link to="/">Home</Link>
         <Link to="../AboutPage">About Me</Link>
     </nav>         
-    <LogoutButton />
-    </div>
   );
 }
 
