d705ecc1a0950bb0df1efd35097d6c2dc273c51b
drewleean1 <drewleean@gmail.com>
2023-11-24 07:25:00 -0700

    add logout button

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LogoutButton from './LogoutButton';
 
 
 function Nav() {
   return (
-    <nav className="appNav">
+    <div className="ExpenseHeader">
+          <nav className="appNav">
         <Link to="/">Home</Link>
         <Link to="../AboutPage">About Me</Link>
-    </nav>
+    </nav>         
+    <LogoutButton />
+    </div>
   );
 }
 
